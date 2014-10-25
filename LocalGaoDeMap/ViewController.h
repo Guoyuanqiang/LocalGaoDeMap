86e89bca9615de5adbd02f3e7e149b8caa15f60f
guoyuanqiang <guoyuanqiang2008@126.com>
2014-10-25 13:58:11 +0800

    Initial Commit

---
+++ LocalGaoDeMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalGaoDeMap
+//
+//  Created by 郭远强 on 14-10-25.
+//  Copyright (c) 2014年 郭远强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocalGaoDeMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocalGaoDeMap
+//
+//  Created by 郭远强 on 14-10-25.
+//  Copyright (c) 2014年 郭远强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
